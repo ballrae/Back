864d0fa4e244ec5653c98a19977c228308a00b0c
11yubin <11yubine@gmail.com>
2025-09-17 05:04:06 +0900

    fix: 중계 team_id 코드 수정

--- ballrae_backend/settings.py
+++ ballrae_backend/settings.py
@@ -27,7 +27,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 AUTH_USER_MODEL = 'users.User'
 
 
